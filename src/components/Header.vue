e3d08fcbd08d2f7e5fd069f40a41700ec4148962
tunagohan <kazuxtuna@gmail.com>
2022-05-08 16:35:57 +0900

    リンクの対応

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,12 +8,12 @@
               <a
                 href="/find_owner"
                 class="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700"
-                >NFT Owner's</a
+                >Check NFT Owner's</a
               >
               <a
                 href="/multi_transfer"
                 class="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                >工事中</a
+                >Bulk Transfer</a
               >
             </div>
           </div>
